25d4cd85e99de35c51f9dbf4954118fc81dbdd61
ellofae <sergei.bykovskiy2003@gmail.com>
2023-04-20 13:14:30 +0300

    Server side timeout added v1.1.0

--- src/server.go
+++ src/server.go
@@ -46,6 +46,7 @@ var NameServersData = make([]NSData, 0)
 var MailServersData = make([]MXData, 0)
 
 var serverTimeout = time.Duration(time.Second)
+var clientTimeout = time.Duration(time.Second)
 
 // Saving and loading data
 func saveData(DATA string, SomeStruct interface{}) error {
